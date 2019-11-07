82f639ef122ca9fd1e55b75526394641a2ea5add
Ali Mahdipour-Shirayeh <ali.mahdipour@gmail.com>
2019-11-07 01:46:11 -0500

    Add files via upload

---
+++ heatmap_break_glist.R
@@ -0,0 +1,30 @@
+
+heatmap_break_glist <- function(CNV.mat2){
+  
+  gen.loc <- read.csv( "./10XGenomics_gen_pos_GRCh38-1.2.0.csv", header=TRUE)
+  gene.list <- which( as.matrix(gen.loc)[,1] %in% as.matrix(colnames(CNV.mat2)))
+  assoc.chr <-  as.matrix(gen.loc[ gene.list, 2]) # as.matrix(mapply( as.matrix(gen.loc[ gene.list, 2]), FUN = as.numeric) )
+  
+  break.glist <- rep(0, 24)
+ 
+  for(i in 1: 22){ 
+    break.glist[i] <- min( which(assoc.chr == i))
+  }
+  ##
+  if( length(which(rownames(assoc.chr) == "X"))  == 0 ){
+    break.glist[23] <- ncol(CNV.mat2)
+  } else {
+    break.glist[23] <- min( which(rownames(assoc.chr) == "X"))
+  }
+  ##
+  if( length(which(  rownames(assoc.chr) == "Y")) == 0 ){
+    break.glist[24] <- ncol(CNV.mat2)
+  } else {
+    break.glist[24] <- min( which(rownames(assoc.chr) == "Y"))
+  }
+  
+  
+  
+return(break.glist)
+  
+}
---
+++ heatmap_break_gloc.R
@@ -0,0 +1,50 @@
+
+
+heatmap_break_gloc <- function( M_origin ){
+
+
+## number of segments on the genome
+No_Intrvl <- 1000
+
+############
+minn <- matrix(0, ncol=24, nrow=1)
+maxx <- matrix(0, ncol=24, nrow=1)
+
+for(i in 1: 22){ 
+  minn[1,i] <- as.numeric(min(M_origin[which(as.matrix(M_origin[, 2]) == i) , 3]) )
+  maxx[1,i] <- as.numeric(max(M_origin[which(as.matrix(M_origin[, 2]) == i) , 3]) )
+}
+minn[1,23] <- as.numeric(min(M_origin[which(as.matrix(M_origin[, 2]) == "X") , 3]) )
+maxx[1,23] <- as.numeric(max(M_origin[which(as.matrix(M_origin[, 2]) == "X") , 3]) )
+
+minn[1,24] <- as.numeric(min(M_origin[which(as.matrix(M_origin[, 2]) == "Y") , 3]) )
+maxx[1,24] <- as.numeric(max(M_origin[which(as.matrix(M_origin[, 2]) == "Y") , 3]) )
+
+Total_length <- sum( (maxx[1,1:24]-minn[1,1:24]) + 1 ) 
+Seg_size <- ceiling( Total_length/No_Intrvl )
+
+########
+
+MaxNo_intervals <- ceiling(max((maxx[1,1:24]-minn[1,1:24])+ 1)/Seg_size)
+Intrvls <- matrix(0, nrow = 24, ncol = MaxNo_intervals)
+Min_chr <- rep(0,25)
+
+Min_chr[1] <- 0
+for(i in 1:24){
+  StartEnd <- seq(minn[1,i] , maxx[1,i] , Seg_size)
+  Intrvls[i, 1:length(StartEnd) ]  <-  t(as.matrix( StartEnd )) + Min_chr[i]
+  Min_chr[i+1]  <-  maxx[1,i]              
+}
+
+
+
+break.gloc <- rep(0,24)
+break.gloc[1] <- 1
+
+for(i in 2: 24){ 
+  break.gloc[i] <- length( Intrvls[1:(i-1),][ Intrvls[1:(i-1),] != 0])
+}
+
+return(break.gloc)
+
+}
