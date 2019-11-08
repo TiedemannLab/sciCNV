bf06a5a7fbe92ab464fb2c652ad828b5ebee8d46
Ali Mahdipour-Shirayeh <ali.mahdipour@gmail.com>
2019-11-08 14:50:47 -0500

    Update sciCNV.R

--- sciCNV Analysis/sciCNV.R
+++ sciCNV Analysis/sciCNV.R
@@ -1,5 +1,6 @@
 
 
+
 #######################################################################################
 ######                             sciCNV Method                                ####### 
 ######             ((  Merging MA and no-MA with baseline Correction  ))        #######
@@ -7,6 +8,7 @@
 ######                       copyright@AliMahdipourShirayeh                     #######
 ######                 Tiedemann Lab - Princess Margaret Cancer centre          #######
 #######################################################################################
+
 
 # norm.mat: Matrix of normalized single-cell gene expression (e.g. normalized scRNAseq). Formatted as follows: 
 #       1st column = gene name list. 
@@ -21,6 +23,7 @@
 #       Default = 0. Ideal setting: a fraction representing the net genomic change from diploidy (using a positive fraction for gain and a negative fraction for net genomic loss).
 #       If the CNV profile is unknown, run the CNV analysis with default zero setting, review the preliminary CNV profile, and consider re-running with baseline ccorrection.
 # baseline_adj: The baseline adjustment is only applied to test cells if it is TRUE. Default is FALSE. 
+
 
 sciCNV <- function(norm.mat, 
                    No.test, 
@@ -61,10 +64,10 @@ sciCNV <- function(norm.mat,
   
   
   ## Assigning chromosome number to each gene sorted based on chromosme number, starts and ends 
-  Gen.Loc <- read.csv( "./10XGenomics_gen_pos_GRCh38-1.2.0.csv", header=TRUE)
-  Spec.genes <- which(as.matrix(Gen.Loc[,1]) %in% as.matrix(rownames(norm.mat)))
-  Gen.Loc <-  Gen.Loc1[Spec.genes, c(1,2)]
-  dim(Gen.Loc)
+  gen.Loc <- read.table( "./10XGenomics_gen_pos_GRCh38-1.2.0.txt", sep = '\t', header=TRUE)
+  Spec.genes <- which(as.matrix(gen.Loc[,1]) %in% as.matrix(rownames(norm.mat)))
+  gen.Loc <-  gen.Loc[Spec.genes, c(1,2)]
+  
   
   ## Selecting genes with expression in at least 2% of cells (this can be changed due to biology/natural features of the test sample)
   ## and attaching the average expressions of the control samples per gene
@@ -79,16 +82,16 @@ sciCNV <- function(norm.mat,
   
   
   ######
-  norm.mat1 <- cbind(Gen.Loc, as.matrix(norm.mat), as.matrix(Ave_NCs) )
+  norm.mat1 <- cbind(gen.Loc, as.matrix(norm.mat), as.matrix(Ave_NCs) )
+  rownames(norm.mat1) <- rownames(norm.mat)
   
   ## Variable that excludes infrequently detected genes, detected below a minimum % of cells (assessed across test and control cells).
   ##  This enriches the analysis for informative genes.
   percent <- 0.02
   MSC1 <- norm.mat1[which(rowSums(norm.mat != 0) >= ncol(norm.mat)*percent ), ] 
   colnames(MSC1) <- c("genes","chromosomes", colnames(norm.mat),"AveNCs") 
-  chr.n <- MSC1[, 2]
+  #chr.n <- sort(MSC1[ , 2], decreasing=FALSE)
   MSC <- MSC1[ , -c(1,2)]
-  dim(MSC)
   
   ################################
   ## infered CNV function
@@ -98,58 +101,6 @@ sciCNV <- function(norm.mat,
   # Average expression of control cells
   mean.cntrl <- as.matrix(as.numeric(MSC[, ncol(MSC)]) ) 
   
-  ## defining parameters
-  resolution <- nrow(MSC)/(50*sharpness)
-  P12 <- floor(resolution)        
-  P31 <- P12/2;      E22 <- 1
-  E23 <- 1;          O51 <- 0.985
-  O52 <- 1 - O51;    P31 <- P12/2
-  E22 <- 1;          E23 <- 1
-  O25 <- 0.78;       O26 <- 0.2
-  C297 <- resolution/10  # = 4  #8
-  C298 <- 2;         J311 <- 1
-  m <- 1.0001
-  t <-  0.0002
-  Lambda <- 0.00001
-  
-  
-  ## Generating relative expression vs. negative control
-  FF <- rep(0, nrow(MSC))
-  AW <- rep(0, nrow(MSC))
-  BD <- rep(0, nrow(MSC))
-  G <- as.matrix(seq(1,nrow(MSC),1))
-  #rownames(chr.n) <- G
-  
-  FF[1] <- 1
-  for(i in 2:nrow(MSC)){
-    if( chr.n[i] == chr.n[i-1]){ 
-      FF[i] <-  FF[i-1] + 1
-    } else 
-      FF[i] <-  1
-  }
-  
-  AW[1] <- P12
-  for(i in 2:nrow(MSC)){
-    if( chr.n[i] == chr.n[i-1]){ 
-      if( AW[i-1] >0 ){ 
-        AW[i] <-  AW[i-1] -1 
-      } else 
-        AW[i] <-  0
-    } else 
-      AW[i] <-  P12
-  } 
-  
-  
-  BD[1] <- P12
-  for(i in seq(nrow(MSC)-1,1,-1) ){
-    if( chr.n[i] == chr.n[i+1]){ 
-      if( BD[i+1] > 0 ){ 
-        BD[i] <-  BD[i+1] -1 
-      } else 
-        BD[i] <-  0
-    } else 
-      BD[i] <-  P12
-  }
   
   
   ## sciCNV for test cells  
@@ -158,7 +109,8 @@ sciCNV <- function(norm.mat,
                                        mean.cntrl = mean.cntrl,  
                                        sharpness = sharpness, 
                                        baseline_adj = baseline_adj,
-                                       baseline = baseline )}   )
+                                       baseline = baseline,
+                                       new.genes = rownames(MSC1))}   )
   
   
   ## sciCNV for control cells
@@ -167,7 +119,8 @@ sciCNV <- function(norm.mat,
                                        mean.cntrl = mean.cntrl,
                                        sharpness = sharpness, 
                                        baseline_adj = baseline_adj,
-                                       baseline = 0 )}   )
+                                       baseline = 0,
+                                       new.genes = rownames(MSC1))}   )
   
 
   V7Alt <- cbind(V7Alt1, V7Alt2)
