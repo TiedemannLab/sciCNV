760aff8176dada3c1f2cffa8b6d428c24a5ac5df
Ali Mahdipour-Shirayeh <ali.mahdipour@gmail.com>
2020-01-20 13:02:28 -0500

    Update Opt_MeanSD_RTAM1.R

--- sciCNV-Analysis/Opt_MeanSD_RTAM1.R
+++ sciCNV-Analysis/Opt_MeanSD_RTAM1.R
@@ -135,7 +135,7 @@ Opt_MeanSD_RTAM1 <- function(Normalized_log,
 
 
 
-Scaled_Normalized_log <- as.matrix(Scaled_Normalized_log)
+  Scaled_Normalized_log <- as.matrix(Scaled_Normalized_log)
   comm.expr <-   as.matrix(Scaled_Normalized_log[which(rowSums(Scaled_Normalized_log[ ,seq(1, ncol(AA), 1)]!=0) > 0.95*ncol(Scaled_Normalized_log) ), ] )
   MEAN_comm <- matrix(0, ncol=ncol(AA), nrow=1)
   for(j in 1:ncol(AA)){
