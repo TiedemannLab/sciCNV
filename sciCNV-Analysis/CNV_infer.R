80485ed883dc3df5fb0cd30196672ea5f400e450
TiedemannLab <57154620+TiedemannLab@users.noreply.github.com>
2021-08-22 19:49:41 -0400

    Update CNV_infer.R

--- sciCNV-Analysis/CNV_infer.R
+++ sciCNV-Analysis/CNV_infer.R
@@ -1,5 +1,8 @@
 
 
+
+
+
 #######################################################################################
 ######                             CNV_infer function                           ####### 
 ######          for Single Cell Inferred Copy Number Variation (sciCNV)         #######
@@ -11,19 +14,24 @@
 #
 # Definitions:
 # ss.expr: scRNA-seq based expression for each test/control cell
-# mean.cntrl: The average gene expression of control cells
+# mean.ctrl: The average gene expression of control cells
 # chr.n: List of chromosome numbers associated with the list of genes
-# sharpness: Adjusts the resolution used for the sciCNV-curve calculation. Default =1.0 (range approximately 0.6-1.4).
+# Resolution: Adjusts the resolution, nrow(MSC)/(50*sharpness), used for the sciCNV-curve calculation. Default sharpness is =1.0 (best sharpnesses range between 0.6-1.4).
 # baseline: An optional correction to adjust the CNV zero setpoint (copy number gain =0) which is otherwise the median CNV of all genes. 
 #           Default = 0 and typical range is between -0.5 and 0.5.
 # baseline_adj: The baseline adjustment is only applied to test cells if it is TRUE. Default is FALSE.
+# P12: The variable which is associated to resolution, is automatically calculated and is used for CNV calling.
+# mat.fab: is a merged matrix of FF, AW, BD vectors per cell. These vectors are used to calculate the relative expression of test to control. 
 
 CNV_infer  <- function( ss.expr , 
-                        mean.cntrl,  
-                        sharpness, 
-                        baseline_adj,
-                        baseline,
-                        new.genes
+                        mean.ctrl,
+                        gen.Loc , 
+                        resolution, 
+                        baseline_adj = FALSE,
+                        baseline = 0,
+                        chr.n,
+                        P12,
+                        mat.fab
 ){ 
   
   ## argument value/validation
@@ -48,14 +56,7 @@ CNV_infer  <- function( ss.expr ,
     BLC <- 0
   }
   
-  ## Assigning chromosome number to each gene
-  gen.Loc <- read.table( "../sciCNV/Dataset/10XGenomics_gen_pos_GRCh38-1.2.0.txt", sep = '\t', header=TRUE)
-  chr.n <- as.matrix( gen.Loc[which(as.matrix(gen.Loc[,1]) %in% new.genes), 2])
-
-  
-  ## defining parameters
-  resolution <- nrow(ss.expr)/(50*sharpness)
-  P12 <- floor(resolution)        
+  ## defining parameters        
   P31 <- P12/2;      E22 <- 1
   E23 <- 1;          O51 <- 0.985
   O52 <- 1 - O51;    P31 <- P12/2
@@ -66,57 +67,24 @@ CNV_infer  <- function( ss.expr ,
   m <- 1.0001
   t <-  0.0002
   Lambda <- 0.00001
+  row_n <- nrow(ss.expr)
   
-  
-  ## Generating relative expression vs. control
-  FF <- rep(0, nrow(ss.expr))
-  AW <- rep(0, nrow(ss.expr))
-  BD <- rep(0, nrow(ss.expr))
-  G <- as.matrix(seq(1,nrow(ss.expr),1))
-  #rownames(chr.n) <- G
-  
-  FF[1] <- 1
-  for(i in 2:nrow(ss.expr)){
-    if( chr.n[i] == chr.n[i-1]){ 
-      FF[i] <-  FF[i-1] + 1
-    } else 
-      FF[i] <-  1
-  }
-  
-  AW[1] <- P12
-  for(i in 2:nrow(ss.expr)){
-    if( chr.n[i] == chr.n[i-1]){ 
-      if( AW[i-1] >0 ){ 
-        AW[i] <-  AW[i-1] -1 
-      } else 
-        AW[i] <-  0
-    } else 
-      AW[i] <-  P12
-  } 
-  
-  
-  BD[1] <- P12
-  for(i in seq(nrow(ss.expr)-1,1,-1) ){
-    if( chr.n[i] == chr.n[i+1]){ 
-      if( BD[i+1] > 0 ){ 
-        BD[i] <-  BD[i+1] -1 
-      } else 
-        BD[i] <-  0
-    } else 
-      BD[i] <-  P12
-  }
-  
+  ## Recalling FF, AW, BD vectors and defining G matrix to start with average relative expression of test to control
+  G <- as.matrix(seq(1,row_n,1))
+  FF <- mat.fab[,1]
+  AW <- mat.fab[,2]
+  BD <- mat.fab[,3]
   
   #---------------  T: moving average of gene expression in control cells & U: moving average of expression in test cell
-  TT <- rep(0, nrow(ss.expr)); U <- rep(0, nrow(ss.expr))
-  max_1 <- pmax( as.matrix(AW - P12), matrix(-floor(P12/C297), nrow(ss.expr)) )
-  min_1 <- pmin( as.matrix(P12 - BD), matrix(floor(P12/C297), nrow(ss.expr)) )
+  TT <- rep(0, row_n); U <- rep(0, row_n)
+  max_1 <- pmax( as.matrix(AW - P12), matrix(-floor(P12/C297), row_n) )
+  min_1 <- pmin( as.matrix(P12 - BD), matrix(floor(P12/C297), row_n) )
   
-  for( i in 1:nrow(ss.expr) ){
+  for( i in 1:row_n ){
     bb <- seq(i + max_1[i], i + min_1[i] , 1)
     bb <- bb[! is.na(ss.expr[bb])]
     TT[i] <- mean( ss.expr[bb] )
-    U[i] <- mean( mean.cntrl[bb] )
+    U[i] <- mean( mean.ctrl[bb] )
     
   }
   
@@ -124,36 +92,36 @@ CNV_infer  <- function( ss.expr ,
   #---------------------- V: ma-Wratio (weighted disparity score comparing gene expression in test and control cells within the chromosome segment defined by the moving averages)
   V <- (TT - U)/(TT + U + Lambda )
   
-  P100 <- quantile( as.matrix(V)[seq(1,nrow(ss.expr),1)], 0.5 - BLC )
+  P100 <- quantile( as.matrix(V)[seq(1,row_n,1)], 0.5 - BLC )
   WW <- V - P100
   
   
   #---------------  X: moving average of test cell, before weighting. & Y: moving average of control cells, before weighting 
-  X <- rep(0, nrow(ss.expr)); Y <- rep(0, nrow(ss.expr))
-  max_2 <- pmax( as.matrix(AW - P12), matrix(-floor(P12/C298), nrow(ss.expr)) )
-  min_2 <- pmin( as.matrix(P12 - BD), matrix(floor(P12/C298), nrow(ss.expr)) )
+  X <- rep(0, row_n); Y <- rep(0, row_n)
+  max_2 <- pmax( as.matrix(AW - P12), matrix(-floor(P12/C298), row_n) )
+  min_2 <- pmin( as.matrix(P12 - BD), matrix(floor(P12/C298), row_n) )
   
-  for(i in 1:nrow(ss.expr)){
+  for(i in 1:row_n){
     bb <- seq(i + max_2[i], i + min_2[i], 1)
     bb <- bb[! is.na(ss.expr[bb])]
     X[i] <- mean( ss.expr[bb] )
-    Y[i] <- mean( mean.cntrl[bb] )
+    Y[i] <- mean( mean.ctrl[bb] )
   }
   
   #---------------------- Z: ma-Wratio (weighted expression disparity score comparing gene expression in test and control cells within the chromosome segment defined by the moving averages) 
   #-----------------------AA: median normalized Z. AB: Linearized solution of AA from quadratic equation (aka W-lin)
   Z <- (X - Y)/(X + Y + Lambda)
   
-  P101 <- median( Z[seq(1,nrow(ss.expr),1) ]) 
+  P101 <- median( Z[seq(1,row_n,1) ]) 
   AA <- Z - P101
   AB <- 3.3222*(AA)^4 + 5.6399*(AA)^3 + 4.2189*(AA)^2 + 3.8956*(AA)
   
   #---------------------- ACC: ma-DMS ("Demoncratic moving score" [1 CNV vote per gene]- calculated on post moving average post median normalized data)
   
-  ACC <- rep(0, nrow(ss.expr))
+  ACC <- rep(0, row_n)
   
   ACC[1] <- 0
-  for(i in 2:nrow(ss.expr)){
+  for(i in 2:row_n){
     if( WW[i] > 0){ 
       ACC[i] <-  ACC[i-1] + 1
     } else if(WW[i] < 0){ 
@@ -164,17 +132,17 @@ CNV_infer  <- function( ss.expr ,
   
   #---------------  NEW: AC: ma-DMS (on post MovAve median)  &  DA: ma-Wratio  non-Moving Ave. +/-  
   AC <- ACC + ( BLC * G )
-  DA <- (ss.expr - mean.cntrl) %/% (ss.expr + mean.cntrl + Lambda)
+  DA <- (ss.expr - mean.ctrl) %/% (ss.expr + mean.ctrl + Lambda)
   
   
   #---------------------- AC: non-ma DMS
   V56 <- length( DA[ DA > 0 ] )
   V57 <- length( DA[ DA < 0 ] )
-  V60 <- (V56-V57)/nrow(ss.expr)
-  DD <-  rep(0, nrow(ss.expr))
+  V60 <- (V56-V57)/row_n
+  DD <-  rep(0, row_n)
   
   DD[1] <- 0
-  for(i in 2:nrow(ss.expr)){
+  for(i in 2:row_n){
     if( DA[i] > 0){ 
       DD[i] <-  DD[i-1] + 1 - V60
     } else if(DA[i] < 0){ 
@@ -191,34 +159,34 @@ CNV_infer  <- function( ss.expr ,
   DH <- (AC*0.66) + (DG*0.34)
   
   #---------------------- AD: V7alt[ma- and non-ma] Centrograde : Slope of AC (DMS)
-  DI <- rep(0, nrow(ss.expr))
+  DI <- rep(0, row_n)
   
   
-  for(i in 1:nrow(ss.expr)){
-    if( (BD[i] < P31) & (AW[i] < P31) ){
+  for(i in 1:row_n){
+    if( (BD[i] < P31) && (AW[i] < P31) ){
       aaa <- seq(i - P31 ,i + P31,1)
       aaa <- aaa[! is.na(DH[aaa])]
       Ya <- as.matrix(DH[aaa])
       summary(lmResult <- lm(Ya~aaa))
       DI[i] <- as.matrix(coef(lmResult)["aaa"][[1]])
       
-    } else if( (BD[i] < P31) & (AW[i] >= P31) ){
-      if(  (AW[i] - P12) < P31 ){ 
+    } else if( (BD[i] < P31) && (AW[i] >= P31) ){
+      if(  (AW[i] - P12) < P31 ){
         aaa <- seq(i + AW[i] - P12, i + P31 , 1)
         aaa <- aaa[! is.na(DH[aaa])]
         Ya <- as.matrix(DH[aaa])
         summary(lmResult <- lm(Ya~aaa))
         DI[i] <- as.matrix(coef(lmResult)["aaa"][[1]])
         
-      } else 
+      } else
         aaa <- seq(i + AW[i] - P12, i + P31 , -1)
       aaa <- aaa[! is.na(DH[aaa])]
       Ya <- as.matrix(DH[aaa])
       summary(lmResult <- lm(Ya~aaa))
       DI[i] <- as.matrix(coef(lmResult)["aaa"][[1]])
       
-    } else if( (BD[i] >= P31) & (AW[i] < P31)  ){
-      if( ( - P31) < ( P12 - BD[i]) ){ 
+    } else if( (BD[i] >= P31) && (AW[i] < P31)  ){
+      if( ( - P31) < ( P12 - BD[i]) ){
         aaa <- seq(i - P31, i + P12 - BD[i] , 1)
         aaa <- aaa[! is.na(DH[aaa])]
         Ya <- as.matrix(DH[aaa])
@@ -232,15 +200,15 @@ CNV_infer  <- function( ss.expr ,
       summary(lmResult <- lm(Ya~aaa))
       DI[i] <- as.matrix(coef(lmResult)["aaa"][[1]])
       
-    } else if( (BD[i] >= P31) & (AW[i] >= P31)  ){
-      if( (P12 - BD[i]) < (AW[i] -P12) ){ 
+    } else if( (BD[i] >= P31) && (AW[i] >= P31)  ){
+      if( (P12 - BD[i]) < (AW[i] -P12) ){
         aaa <- seq(i + P12 - BD[i], i + AW[i] -P12 , 1)
         aaa <- aaa[! is.na(DH[aaa])]
         Ya <- as.matrix(DH[aaa])
         summary(lmResult <- lm(Ya~aaa))
         DI[i] <- as.matrix(coef(lmResult)["aaa"][[1]])
         
-      } else if( (P12 - BD[i])==(AW[i] - P12) ){ 
+      } else if( (P12 - BD[i]) == (AW[i] - P12) ){
         DI[i] <- 0
       } else{
         aaa <- seq(i + P12 - BD[i], i + AW[i] - P12 , -1)
@@ -255,10 +223,10 @@ CNV_infer  <- function( ss.expr ,
   
   
   #---------------------- AF: maV7-alt x maW-lin curve & AG: maV7-alt x maW-lin curve with Manual Scale
-  AF <- rep(0, nrow(ss.expr))
+  AF <- rep(0, row_n)
   
-  for(i in 1:nrow(ss.expr)){
-    if( (! is.na(DI[i]*AB[i])) & (DI[i]*AB[i] > 0) ){
+  for(i in 1:row_n){
+    if( (! is.na(DI[i]*AB[i])) && (DI[i]*AB[i] > 0) ){
       AF[i] <- DI[i]*abs(AB[i])
     } else
       AF[i] <- 0
@@ -275,3 +243,5 @@ CNV_infer  <- function( ss.expr ,
   
   
 }
+
+
